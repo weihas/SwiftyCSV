858b7ad504c0e18357db5fcb850bf6c562811859
weihas <weihas@icloud.com>
2024-03-05 16:42:32 +0800

    [feat]add Line init

--- Sources/SwiftyCSV/CSVLine.swift
+++ Sources/SwiftyCSV/CSVLine.swift
@@ -9,8 +9,16 @@ import Foundation
 
 /// Represents a single line in a CSV file, encapsulating the logic for parsing and assembling CSV fields.
 public struct CSVLine: RawRepresentable {
-    /// An array of strings, each representing a field within the CSV line.
-    public var fields: [String] = []
+    
+    /// An array of strings, each representing a field within the CSV line. This is the primary storage for the line's data.
+    public var fields: [String]
+    
+    /// Initializes a `CSVLine` instance directly with an array of strings representing the fields of the CSV line.
+    /// This initializer can be used when the fields are already separated and no parsing of a raw string representation is necessary.
+    /// - Parameter fields: An array of strings, where each string is a field in the CSV line.
+    public init(fields: [String]) {
+        self.fields = fields
+    }
     
     /// Initializes a `CSVLine` instance from a raw string representation of a CSV line.
     /// The raw string is split into fields based on CSV formatting rules, handling quotes and commas appropriately.
